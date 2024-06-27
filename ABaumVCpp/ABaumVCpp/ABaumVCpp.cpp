e9467a25a298a56da4c9cf0bf29dd5e0f1c563e5
Reinhold Brant <reinholdb_@MacBook-Air-von-Reinhold.local>
2024-06-27 19:01:52 +0200

    int, op und braket support, nicht getestet

--- ABaumVCpp/ABaumVCpp/ABaumVCpp.cpp
+++ ABaumVCpp/ABaumVCpp/ABaumVCpp.cpp
@@ -15,16 +15,13 @@ using namespace std;
  * Parameter: argc Anzahl der Argumente
  *            argv Kommandozeilen-Argumente
  */
-void main(int argc, char* argv[]) 
+int main(int argc, char* argv[]) 
 {
     string exp("");
  
-	// Hilfsausgabe: Kommandozeilenargumente
-	/* 
 	cout << "argc = " << argc << endl;
 	for (int i = 0; i < argc; i++)
 	   cout << "argv[" << i << "] = " << argv[i] << endl; 
-    */
 	
 	if (argc > 1) exp = argv[1];
     for (int i = 1; i < argc; i++) 
@@ -37,12 +34,16 @@ void main(int argc, char* argv[])
     else 
 	{
         char mode = argv[1][0];
-        if (mode == '<' || mode == '|' || mode == '>') 
+		// REINHOLD: '<' = Prefix, '|' = Infix (Standard), '>' = Postfix sind tatsächlich schwer (glaube ich)
+		// weil der Befehl "programm.exe >2+2" nicht den Baum mit 2+2 erzeugt, sondern programm.exe in eine neue Datei
+		// 2+2 schreiben würde..deswegen fuers erste die chars fuer die modi geaendert
+        if (mode == 'P' || mode == 'I' || mode == 'O') 
 			exp = exp.substr(1);
         else 
-			mode = '|'; // Standard: Infix
+			mode = 'I'; // Standard: Infix
         
 		Evaluator e;
+		std::cout << std::endl << "mode" << mode << std::endl;
 		e.evaluate(exp, mode);
     }
 }
